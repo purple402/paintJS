a1f77093e44b6f0695aa2aeedd904571bb183783
Joy Lee <purple402@naver.com>
2021-03-17 16:29:33 +0900

    add comment

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
     if(!painting) {
-        ctx.beginPath();
-        ctx.moveTo(x, y);
+        ctx.beginPath(); // 경로 생성
+        ctx.moveTo(x, y); // 선 시작 좌표
     } else {
-        ctx.lineTo(x, y);
-        ctx.stroke();
+        ctx.lineTo(x, y); // 선 끝 좌표
+        ctx.stroke(); // 선 그리기
     }
 }
 
