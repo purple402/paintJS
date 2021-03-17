45aed7813928866b2f7cf0fab60cf5ebb5324b63
Joy Lee <purple402@naver.com>
2021-03-17 17:55:19 +0900

    update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
-const colors =document.getElementsByClassName("jsColor");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 500;
 canvas.height = 500;
@@ -40,6 +40,6 @@ if(canvas) {
     canvas.addEventListener('mousedown', startPainting);
     canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
+Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
 }
 
-Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
