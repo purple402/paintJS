8f576ef2d416632cd81856331ed1f6588e3ed9eb
Joy Lee <purple402@naver.com>
2021-03-19 13:51:25 +0900

    make save button

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const save = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 500;
@@ -74,6 +75,13 @@ function handleCM(event) {
     event.preventDefault();
 }
 
+function handleSaveClick() {
+    const download =
+    const dataURL = canvas.toDataURL();
+
+     
+}
+
 if(canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
     canvas.addEventListener('mousedown', startPainting);
@@ -93,4 +101,8 @@ if(range) {
 
 if(mode) {
     mode.addEventListener("click", handleModeClick);
+}
+
+if(save) {
+    save.addEventListener("click", handleSaveClick);
 }
